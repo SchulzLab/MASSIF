d945d7a5293569b87023b5406c8c8ab3b9eadace
Nina Baumgarten <ninab@srv-97-32.mmci.uni-saarland.de>
2019-01-11 15:39:19 +0100

    small chances in testSmall.sh

--- testSmall.sh
+++ testSmall.sh
@@ -13,13 +13,11 @@ path_to_CentriMo=$1
 	echo "using domain info as prediction"
  	time python mainScript.py  tests/transfac_testSmall.txt ${path_to_CentriMo} tests/seq_testSmall/ testSmall tests/biologicalSignal_testSmall/
 
-	echo "check if result is equal to provided results - If it print any line before ..... something went wrong"
-	diff --suppress-common-lines result_fisherMethod_testSmall.txt  tests/result_fisherMethod_testSmall.txt
-	echo "....."
+#	echo "check if result is equal to provided results - If it print any line before ..... something went wrong"
+#	diff --suppress-common-lines result_fisherMethod_testSmall.txt  tests/result_fisherMethod_testSmall.txt
+#	echo "....."
 
 	echo "using domain info as filter"
  	time python mainScript_FilteringFisherMethod.py  tests/transfac_testSmall.txt ${path_to_CentriMo} tests/seq_testSmall/ testSmallFisher tests/biologicalSignal_testSmall/ RandomMotifs/pvalue_0.001_ThresholdDomainInfo.txt 
 
-	echo "check if result is equal to provided results - If it print any line before ..... something went wrong"
-	diff --suppress-common-lines result_fisherMethod_testSmallFisher.txt  tests/result_fisherMethod_testSmallFisher.txt
-	echo "....."
+	#diff --suppress-common-lines result_fisherMethod_testSmallFisher.txt  tests/result_fisherMethod_testSmallFisher.txt
